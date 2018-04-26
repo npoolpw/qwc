4e2a6a4aaa6efb552f1b8f240332aa7fb4fa0867
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-04-26 16:12:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-var api = "http://qwc.npool.pw:8116";
+var api = "http://ca.npool.pw:8116";
 
-var poolHost = "qwc.npool.pw";
+var poolHost = "ca.npool.pw";
 
-var blockExplorer = "http://blockexplorer.pw/qwc";
+var blockExplorer = "http://xplorer.qwertycoin.org/";
 
 var cryptonatorWidget = ["BTC-USD", "QWC-BTC"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "http://xplorer.qwertycoin.org/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "http://xplorer.qwertycoin.org/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
